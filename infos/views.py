ff26b938d5bd31ca497f4eeb63a527a8f9258ba0
Wiktoria Stasiak <jan.kowalski@example.com>
2024-02-27 11:03:44 +0100

    Poprawa views.py

--- infos/views.py
+++ infos/views.py
@@ -1,14 +1,12 @@
+
 from django.shortcuts import render
-from django.template import Context
 
 def home(request):
-    context = Context({'title': 'Strona główna', 'content': 'Witaj na mojej stronie głównej!'})
-    return render(request, 'infos/home.html', context)
+    return render(request, 'infos/home.html')
 
 def about_me(request):
-    context = Context({'title': 'O mnie', 'content': 'Krótka informacja o mnie. Plus za zdjęcie.'})
-    return render(request, 'infos/about_me.html', context)
+    return render(request, 'infos/about_me.html')
 
 def contact(request):
-    context = Context({'title': 'Kontakt', 'content': 'Jakieś dane kontaktowe. Plus za formularz, w którym można wpisać treść i wysłać przyciskiem.'})
-    return render(request, 'infos/contact.html', context)
+    return render(request, 'infos/contact.html')
+
